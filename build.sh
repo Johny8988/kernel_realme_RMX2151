de79554d1f680c81381ed1b0712e6371171d0c68
Sarthak Roy <sarthakroy2002@gmail.com>
2022-02-25 07:21:54 +0000

    build.sh: Update for evaGCC
    Signed-off-by: Sarthak Roy <sarthakroy2002@gmail.com>

--- build.sh
+++ build.sh
@@ -6,25 +6,28 @@ function compile()
 source ~/.bashrc && source ~/.profile
 export LC_ALL=C && export USE_CCACHE=1
 ccache -M 100G
+git clone --depth=1 https://github.com/StatiXOS/android_prebuilts_gcc_linux-x86_arm_arm-eabi gcc
+git clone --depth=1 https://github.com/StatiXOS/android_prebuilts_gcc_linux-x86_aarch64_aarch64-elf gcc64
 export ARCH=arm64
 export KBUILD_BUILD_HOST=ThunderStorm
 export KBUILD_BUILD_USER="AnupamRoy"
-git clone --depth=1 https://github.com/sarthakroy2002/android_prebuilts_clang_host_linux-x86_clang-6443078 clang
-git clone --depth=1 https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9 los-4.9-64
-git clone --depth=1 https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_arm_arm-linux-androideabi-4.9 los-4.9-32
 
 [ -d "out" ] && rm -rf out || mkdir -p out
 
 make O=out ARCH=arm64 RMX2151_defconfig
 
-PATH="${PWD}/clang/bin:${PATH}:${PWD}/los-4.9-32/bin:${PATH}:${PWD}/los-4.9-64/bin:${PATH}" \
+PATH="${PWD}/gcc/bin:${PATH}:${PWD}/gcc64/bin:/usr/bin:$PATH" \
 make -j$(nproc --all) O=out \
-                      ARCH=arm64 \
-                      CC="clang" \
-                      CLANG_TRIPLE=aarch64-linux-gnu- \
-                      CROSS_COMPILE="${PWD}/los-4.9-64/bin/aarch64-linux-android-" \
-                      CROSS_COMPILE_ARM32="${PWD}/los-4.9-32/bin/arm-linux-androideabi-" \
-                      CONFIG_NO_ERROR_ON_MISMATCH=y
+			CROSS_COMPILE_ARM32=arm-eabi- \
+			CROSS_COMPILE=aarch64-elf- \
+			LD=aarch64-elf-ld.lld \
+			AR=llvm-ar \
+			NM=llvm-nm \
+			OBJCOPY=llvm-objcopy \
+			OBJDUMP=llvm-objdump \
+			CC=aarch64-elf-gcc \
+			STRIP=llvm-strip \
+			CONFIG_DEBUG_SECTION_MISMATCH=y
 }
 
 function zupload()
