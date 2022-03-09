d6db2cef4a9ff5de52f05384b2bfe8c71d0c7589
anupamroy777 <github.769@gmail.com>
2022-03-10 00:46:27 +0530

    drivers/misc: mtk/cameraisp: src/mt6785: cam_isp: use USERKEY_STR_LEN size 32 as the source length size is 32 defined here https://github.com/anupamroy777/kernel_realme_RMX2151/blob/Q-gcc/drivers/misc/mediatek/cameraisp/src/mt6785/inc/camera_isp.h#L154 hence lets use 32 length size this fixes: error: strnlen specified bound 128 exceeds source size 32 [-Werror-stringop-overread]
    Signed-off-by: anupamroy777 <github.769@gmail.com>

--- drivers/misc/mediatek/cameraisp/src/mt6785/camera_isp.c
+++ drivers/misc/mediatek/cameraisp/src/mt6785/camera_isp.c
@@ -531,7 +531,7 @@ struct ISP_IRQ_ERR_WAN_CNT_STRUCT {
 };
 
 static int FirstUnusedIrqUserKey = 1;
-#define USERKEY_STR_LEN 128
+#define USERKEY_STR_LEN 32
 
 struct UserKeyInfo {
 	/* name for the user that register a userKey */
