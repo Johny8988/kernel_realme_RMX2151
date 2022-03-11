1e1c42b66b645a3875712161ca2c9582e4068d18
anupamroy777 <github.769@gmail.com>
2022-03-11 16:58:49 +0530

    build_script: switch to proton clang 13

--- build.sh
+++ build.sh
@@ -9,21 +9,19 @@ ccache -M 100G
 export ARCH=arm64
 export KBUILD_BUILD_HOST=ThunderStorm
 export KBUILD_BUILD_USER="AnupamRoy"
-git clone --depth=1 https://github.com/sarthakroy2002/android_prebuilts_clang_host_linux-x86_clang-6443078 clang
-git clone --depth=1 https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9 los-4.9-64
-git clone --depth=1 https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_arm_arm-linux-androideabi-4.9 los-4.9-32
+git clone --depth=1 https://github.com/Kdrag0n/proton-clang.git clang
 
 [ -d "out" ] && rm -rf out || mkdir -p out
 
 make O=out ARCH=arm64 RMX2151_defconfig
 
-PATH="${PWD}/clang/bin:${PATH}:${PWD}/los-4.9-32/bin:${PATH}:${PWD}/los-4.9-64/bin:${PATH}" \
+PATH="${PWD}/clang/bin:${PATH}:${PWD}/clang/bin:${PATH}:${PWD}/clang/bin:${PATH}" \
 make -j$(nproc --all) O=out \
                       ARCH=arm64 \
                       CC="clang" \
                       CLANG_TRIPLE=aarch64-linux-gnu- \
-                      CROSS_COMPILE="${PWD}/los-4.9-64/bin/aarch64-linux-android-" \
-                      CROSS_COMPILE_ARM32="${PWD}/los-4.9-32/bin/arm-linux-androideabi-" \
+		      CROSS_COMPILE="${PWD}/clang/bin/aarch64-linux-gnu-" \
+                      CROSS_COMPILE_ARM32="${PWD}/clang/bin/arm-linux-gnueabi-" \
                       CONFIG_NO_ERROR_ON_MISMATCH=y
 }
 
